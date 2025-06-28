d09127a8864c500f45b5accc53fbe166d07b4967
xKenji <michael.picardal52@gmail.com>
2025-06-28 18:59:36 +0800

    add dark mode switch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div className="p-3 shadow-md mb-3">
-      <h1 className="text-center font-bold text-3xl text-gray-700">Todo</h1>
+    <div className="grid grid-cols-4 p-3 shadow-md mb-3">
+      <h1 className="col-span-2 col-start-2 text-center font-bold text-3xl text-gray-700">
+        Todo
+      </h1>
+      <label className="justify-end w-full inline-flex items-center cursor-pointer">
+        <input
+          type="checkbox"
+          value=""
+          className="sr-only peer"
+          defaultChecked={darkMode}
+          onChange={() => setDarkMode(!darkMode)}
+        />
+        <span className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gray-600 transition-colors"></span>
+        <span className="ml-1 font-medium text-gray-600">
+          {darkMode ? "Dark" : "Light"}
+        </span>
+      </label>
     </div>
   );
 };
