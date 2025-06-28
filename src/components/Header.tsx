e4387180c64a8e2ca233e1c8988b5cb674a91a1a
xKenji <michael.picardal52@gmail.com>
2025-06-29 00:48:55 +0800

    enhance colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,8 +25,8 @@ const Header = () => {
           defaultChecked={darkMode}
           onChange={() => setDarkMode(!darkMode)}
         />
-        <span className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gray-600 transition-colors"></span>
-        <span className="ml-1 font-medium text-gray-600 dark:text-gray-200">
+        <span className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gray-500 transition-colors"></span>
+        <span className="ml-1 font-medium text-gray-500 dark:text-gray-200">
           {darkMode ? "Dark" : "Light"}
         </span>
       </label>
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -24,7 +24,7 @@ const List = ({ list, complete, deleteItem }: Props) => {
           >
             <div className="font-medium">{item.message}</div>
             <div
-              className="delete font-medium text-red-600 hover:text-red-400 dark:text-red-500 dark:hover:text-red-600 transition-colors underline"
+              className="delete font-medium text-red-600 hover:text-red-400 dark:text-red-400 dark:hover:text-red-300 transition-colors underline"
               onClick={(e) => {
                 e.stopPropagation();
                 deleteItem(item);
@@ -39,14 +39,14 @@ const List = ({ list, complete, deleteItem }: Props) => {
         item.complete ? (
           <li
             key={item.id}
-            className="todo flex justify-between p-3 rounded-md bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-600 dark:text-white transition-colors shadow-md cursor-pointer"
+            className="todo flex justify-between p-3 rounded-md bg-gray-200 hover:bg-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:text-white transition-colors shadow-md cursor-pointer"
             onClick={() => complete(item)}
           >
             <div className="font-medium line-through opacity-60">
               {item.message}
             </div>
             <div
-              className="delete font-medium text-red-600 hover:text-red-400 dark:text-red-500 dark:hover:text-red-600 transition-colors underline"
+              className="delete font-medium text-red-600 hover:text-red-400 dark:text-red-400 dark:hover:text-red-300 transition-colors underline"
               onClick={(e) => {
                 e.stopPropagation();
                 deleteItem(item);
